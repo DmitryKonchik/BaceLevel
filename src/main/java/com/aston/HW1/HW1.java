700ab757261569ab1b3eb8ebe3f90bc7d6f74af4
Dmitry <dkonchik@gmail.com>
2025-01-04 16:10:46 +0300

    HW1

--- src/main/java/com/aston/HW1/HW1.java
+++ src/main/java/com/aston/HW1/HW1.java
@@ -1,29 +1,87 @@
 package com.aston.HW1;
 
+
+
 public class HW1 {
 
     // Перевернуть строку и вывести на консоль
     //  String string = "I love Java";
     public static void turnString(String string) {
+        char[] charArray = string.toCharArray();
+        for (int i = charArray.length - 1; i >= 0; i--) {
+            System.out.print(charArray[i]);
+        }
+        System.out.println();
     }
 
     // int[] ints = {1, 2, 2, 3, 4, 5, 5, 6, 7, 8, 8, 9};
     // Удалить дубликаты из массива и вывести в консоль
     public static void getDistinctNumbers(int[] ints) {
+
+        // проверяем на дубликаты и если они есть, вместо дубликата ставим 0
+        int[] newArr1 = ints;
+        for (int i = 0; i < ints.length; i++) {
+
+            for (int j = 0; j < newArr1.length; j++) {
+                if(i != j && ints[i] == newArr1[j]) {
+                    ints[i] = 0;
+                    break;
+                }
+            }
+        }
+
+        // смотрим сколько в нашем массиве повторяющихся значений замененных на 0
+        int countZero = 0;
+        for(int i: ints) {
+            if(i == 0){
+                countZero++;
+            }
+        }
+        //создаем массив с колличеством элементов без учета дубликатов и заполняем его уникальными значениями
+        int[] newArr2 = new int[ints.length - countZero];
+        int j = 0;
+        for(int i: ints) {
+            if(i != 0){
+                newArr2[j] = i;
+                j++;
+            }
+        }
+        // Выводим массив с удаленными дубликатами
+        for(int i: newArr2) {
+            System.out.println(i);
+        }
     }
 
     // Дан массив, заполненный уникальными значениями типа int.
     // int[] arr = {10, 15, 23, 11, 44, 13, 66, 1, 6, 47};
     // Необходимо найти элемент, который меньше максимума, но больше всех остальных.
     public static Integer findSecondMaxElement(int[] arr) {
-        return null;
+        int max = Integer.MIN_VALUE;
+        int secondMax = Integer.MIN_VALUE;
+
+        for (int num: arr) {
+            if(num > max) {
+                secondMax = max;
+                max = num;
+            } else if (num > secondMax && num < max) {
+                secondMax = num;
+            }
+        }
+        return secondMax;
+
+
     }
 
     // Найти длину последнего слова в строке. В строке только буквы и пробелы.
     // "Hello world" - 5
     // "    fly me    to the moon    " - 4
     public static Integer lengthOfLastWord(String string) {
-        return null;
+        if (string == null || string.length() == 0) {
+            return 0; }
+        //удаляет пробелы в начале и конце строки, затем разбивает строку на слова по пробелам
+        String[] words = string.trim().split("\\s+");
+        //возвращает длину последнего слова
+        return words[words.length - 1].length();
     }
 
     // Определить, что строка является палиндромом
@@ -34,7 +92,15 @@ public class HW1 {
     // aba - true
     // 112211 - true
     public static boolean isPalindrome(String string) {
-        return false;
+        //проверяем строку по символам с начала и конца, постепенно сходясь к середине
+        int left = 0;
+        int right = string.length() - 1;
+        while (left < right) {
+            if (string.charAt(left) != string.charAt(right)) {
+                return false; }
+            left++;
+            right--;
+        } return true;
     }
 
 }
--- src/main/java/com/aston/Main.java
+++ src/main/java/com/aston/Main.java
@@ -1,7 +1,25 @@
 package com.aston;
 
+import com.aston.HW1.HW1;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+//        HW1.turnString("I love Java12324421525");
+
+
+//        int[] ints = {10, 10, 1, 1, 1, 2, 2, 3, 4, 5, 5, 6, 7, 8, 8, 9, 9};
+//        HW1.getDistinctNumbers(ints);
+
+
+//        int[] arr = {10, 15, 23, 11, 44, 13, 66, 1, 6, 47};
+//        System.out.println(HW1.findSecondMaxElement(arr));
+
+
+//        String str1 = "   fly me    to the moon      ";
+//        System.out.println(HW1.lengthOfLastWord(str1));
+
+//        String str2 = "1aqbbqa1";
+//        System.out.println(HW1.isPalindrome(str2));
     }
 }
